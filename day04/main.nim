550bbd04984b0c2f66446c5aecaccddceee6a299
letung3105 <15053897+letung3105@users.noreply.github.com>
2023-12-05 15:28:16 +0700

    refactor day 04

--- day04/main.nim
+++ day04/main.nim
@@ -1,60 +1,57 @@
 import std/[cmdline, math, sequtils, streams, strutils]
 
-proc part01(lines: openarray[string]): int =
-  var sum = 0
-  for line in lines:
-    let scratchCard = line.split(":")[1].strip().split("|")
-    var count = 0
-    var havings = newSeq[uint]()
-    for x in scratchCard[0].splitWhitespace():
-      havings.add(x.parseUint())
+type ScratchCard = object
+  havings: seq[uint]
+  winnings: seq[uint]
 
-    for x in scratchCard[1].splitWhitespace():
-      let winning = x.parseUint()
-      for having in havings:
-        if having == winning:
-          count += 1
-          break
+proc parse(card: var ScratchCard, line: string) =
+  let cardData = line.split(":")[1].strip().split("|")
+  card.havings = newSeq[uint]()
+  card.winnings = newSeq[uint]()
+  for x in cardData[0].splitWhitespace():
+    card.havings.add(x.parseUint())
+  for x in cardData[1].splitWhitespace():
+    card.winnings.add(x.parseUint())
 
-    if count > 0: sum += 2 ^ (count - 1)
+proc countMatchings(card: ScratchCard): int =
+  result = 0
+  for winning in card.winnings:
+    for having in card.havings:
+      if having == winning:
+        result += 1
+        break
 
-  sum
+proc part01(cards: openarray[ScratchCard]): int =
+  result = 0
+  for card in cards:
+    let count = card.countMatchings()
+    if count > 0:
+      result += 2 ^ (count - 1)
 
-proc part02(lines: openarray[string]): int =
-  var copies = newSeqWith[uint](lines.len(), 1)
-  for cardIndex, line in lines.pairs():
-    let scratchCard = line.split(":")[1].strip().split("|")
-    var count = 0
-    var havings = newSeq[uint]()
-    for x in scratchCard[0].splitWhitespace():
-      havings.add(x.parseUint())
-
-    for x in scratchCard[1].splitWhitespace():
-      let winning = x.parseUint()
-      for having in havings:
-        if having == winning:
-          count += 1
-          break
-
-    for offset in 1 .. count:
+proc part02(cards: openarray[ScratchCard]): int =
+  result = 0
+  var copies = newSeqWith[uint](cards.len(), 1)
+  for cardIndex, card in cards.pairs():
+    result += copies[cardIndex]
+    for offset in 1 .. card.countMatchings():
       let idx = cardIndex + offset
       if idx >= copies.len(): break
       copies[idx] += copies[cardIndex]
-
-  copies.sum()
 
 proc main() =
   let params = commandLineParams()
   let input = openFileStream(params[0], fmRead)
   defer: input.close()
 
-  var lines = newSeq[string]()
+  var cards = newSeq[ScratchCard]()
   for line in input.lines():
     if line.isEmptyOrWhitespace(): continue
-    lines.add(line)
+    var card: ScratchCard
+    card.parse(line)
+    cards.add(card)
 
-  let res01 = part01(lines)
-  let res02 = part02(lines)
+  let res01 = part01(cards)
+  let res02 = part02(cards)
   echo res01
   echo res02
 
